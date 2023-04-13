0eb282af8603a4cc72fecb5fd89686d4046bb2cb
lucio-souza <117987746+lucio-souza@users.noreply.github.com>
2023-04-12 21:52:06 -0300

    Add files via upload

---
+++ atividade10.java
@@ -0,0 +1,11 @@
+class atividade10{
+    public static void main(String[] args){
+        int cont=0;
+        int x=10;
+        do{
+            System.out.println(x);
+            cont++;
+            x+=10;
+        }while(cont<20);
+    }
+}
